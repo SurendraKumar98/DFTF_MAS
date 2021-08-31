38861910b5b15871290c177ee9b2e83090d2b4ed
SurendraKumar98 <89846302+SurendraKumar98@users.noreply.github.com>
2021-08-31 17:18:00 +0530

    Add files via upload

---
+++ HostelApp.java
@@ -0,0 +1,26 @@
+package day3;
+/**
+*
+* To demonstrate a main class creation with variables, methods
+*
+* Topic: Core Java
+*
+* Day#: 3
+*
+* @author anbarasuv(40102044)
+*
+*/
+
+public class HostelApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Room roj = new Room();
+		roj.setData(22,"Double-room",222.22f,true);
+		roj.displayData();
+	}
+
+}
+/**
+ * 
+ */
